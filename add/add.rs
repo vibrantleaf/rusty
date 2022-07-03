f0a3ce680542c654e2c2f3c9d7579849515f8541
julia <bigowo.digital@outlook.com>
2022-07-03 18:12:28 +0800

    init

---
+++ add/add.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let num = 2;
+    let add = 1;
+    let sum = return_sum(a,  b);
+    println!("{}", sum);
+    }
---
+++ helllo-world/helloworld.rs
@@ -0,0 +1,6 @@
+fn main(){
+    // print hello, world in terminal
+    let hello = "hello";
+    let world = "world";
+    println!("{} {}", hello , world);
+}
